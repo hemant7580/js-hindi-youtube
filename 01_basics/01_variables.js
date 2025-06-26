6b5ae581bb3918ec436abe5bc653246785a76e9d
hemant7580 <hemanttomar7580@gmail.com>
2025-06-26 10:01:22 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId=144553
+let accountEmail="hemant@google.com"
+var accountPassword="12345"
+accountCoty="Jaipur"
+let accountState
+
+// accountId=2// not allowed
+
+/*prefer not to use "var"
+because of issue in block scope and functional scope*/
+
+accountEmail="hc@hc.com"
+accountPasword="21212121"
+accountCity="Bengaluru"
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
