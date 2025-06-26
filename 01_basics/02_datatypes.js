51fa76ceb136be66f33c2d03dbbe724d4220869d
hemant7580 <hemanttomar7580@gmail.com>
2025-06-26 10:13:40 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"Use strict";//treat all js code as newer version
+
+//alert(3+3)  //we are using nodejs ,not browser
+ console.log(3
+    +
+     3)  //code readability
+     
+ console.log("Hemant")
+ 
+ let name="Hemant"
+ let age=18
+ let isLoggedIn=false
+ let state;
+
+
+//  number=>2 to power 53
+//  bigint
+//  string=""
+//  bolean=>true/false
+//  null=>standalone value 
+//  undefined=>
+// symbol=> unique
+
+//object
+
+console.log(typeof undefined);
+console.log(typeof null);
+ 
