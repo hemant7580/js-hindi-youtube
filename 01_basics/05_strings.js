ea203588090c6b6849e191ad2ef6a79738554ada
hemant7580 <hemanttomar7580@gmail.com>
2025-07-01 05:36:46 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,38 @@
+const name = "hemant"
+const repoCount = 50
+
+// console.log(name + repoCount + " value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('hemanthc')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+
+const newString = gameName.substring(-8, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "     hemant     "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+const url = "https://hemant.com/hemant%20tomar"
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('singh'));
+
+const oldName = new String('hemant-hc-com')
+console.log(oldName.split('-'));
