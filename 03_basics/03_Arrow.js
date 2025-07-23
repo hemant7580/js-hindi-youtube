7ad2a79c1a3c8c8e3492726c2dbc2b2db13c6a31
hemant7580 <hemanttomar7580@gmail.com>
2025-07-23 03:58:53 +0000

    Arrow

---
+++ 03_basics/03_Arrow.js
@@ -0,0 +1,47 @@
+const user = {
+    username: "hemant",
+    price: 999,
+    
+    welcomeMessage: function(){
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "hemant"
+//     console.log(this.username);
+// }
+
+// chai();
+
+const chai = () => {
+    let username = "hemant"
+    console.log(this);
+}
+
+// chai();
+
+// const addTwo = (num1,num2) => {
+//     return num1 + num2
+// }
+
+// console.log(addTwo(3,4))
+
+// const addTwo = (num1, num2) => num1+num2
+
+// const addTwo = (num1, num2) => (num1 + num2)
+
+const addTwo = (num1, num2) => ({username: "hemant"})
+
+console.log(addTwo(3, 4))
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
