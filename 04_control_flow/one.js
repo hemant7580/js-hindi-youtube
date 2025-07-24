f3b0330a833059d71c152611c5b013fec3979939
hemant7580 <hemanttomar7580@gmail.com>
2025-07-24 02:35:49 +0000

    If - else statement

---
+++ 04_control_flow/one.js
@@ -0,0 +1,58 @@
+// if
+const isUserloggedIn = true
+
+// if( 2 == "2"){
+//     console.log("executed");
+// }
+
+// const temperature = 41;
+
+// if(temperature < 50){
+//     console.log("less than 50");
+// }
+// else{
+//     console.log("tempetrature is greater than 50");
+// }
+// console.log("Execute");
+
+// <,>,<=,>=,==,!=,===0
+
+// const score = 200
+
+// if(score > 100)
+// {
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+// console.log(`User power: ${power}`);
+
+const balance = 1000
+
+// if(balance > 500) console.log("test"),console.log("tet2");
+
+// if(balance < 500) {
+//     console.log("less than 500");
+// }
+// else if(balance < 750) {
+//     console.log("less than 750");
+// }
+// else if(balance < 900) {
+//     console.log("balance less than 900");
+// }
+// else {
+//     console.log("less than 1200");
+// }
+
+const userLoggedIn = true;
+const debitCard = true;
+const loggedInFromGoogle = false;
+loggedInFromEmail = true;
+
+if(userLoggedIn && debitCard && 2==3)
+{
+    console.log("Allow to buy course");
+}
+
+if(loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
